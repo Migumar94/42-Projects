6d66590c670578fc7cce62828525e2098919b893
Miguel <92090688+Migueluwu@users.noreply.github.com>
2024-11-06 20:46:26 +0100

    Entrega printf

---
+++ printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: migumar2 <migumar2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 18:01:01 by migumar2          #+#    #+#             */
+/*   Updated: 2024/01/26 03:56:29 by migumar2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int		ft_printf(char const *str, ...);
+void	ft_print_c(char c, int *total);
+
+#endif
